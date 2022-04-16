b8274078445560f38046517bdafa1092228ed27c
arsal <arsal.hameed@gmail.com>
2022-04-16 16:30:26 +0400

    v1 module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ProvidersModule } from './providers/providers.module';
 import { CoreModule } from './core/core.module';
 import appConfig from './config/app.config';
+import { V1Module } from './v1/v1.module';
+import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import appConfig from './config/app.config';
     }),
     ProvidersModule,
     CoreModule,
+    V1Module,
+    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
