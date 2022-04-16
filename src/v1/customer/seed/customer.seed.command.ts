e8445bde6f52ec4be8514dd4e1297ca769061265
arsal <arsal.hameed@gmail.com>
2022-04-16 22:41:20 +0400

    mapped data and proper seeder to added data into db

--- src/v1/customer/seed/customer.seed.command.ts
+++ src/v1/customer/seed/customer.seed.command.ts
@@ -1,16 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CustomerService } from '../services/customer.service';
 import { Command } from 'nestjs-command';
+import { customerData } from './data/customer.data';
+export type CustomerDataType = {
+  name: string;
+  locationLatitude: number;
+  locationLongitude: number;
+  numberOfRides: number;
+  rating: number;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+};
 
 @Injectable()
 export class CustomerSeedCommand {
   constructor(private readonly customerService: CustomerService) {}
-
   @Command({
     command: 'seed:customer',
     describe: 'Seed Customer Data',
   })
   async create() {
-    this.customerService.createMany([]);
+    try {
+      const mapped: CustomerDataType[] = [];
+      customerData.map((data) => {
+        delete data.id;
+        mapped.push({
+          ...data,
+          coordinates: {
+            longitude: data.locationLongitude || 0.0,
+            latitude: data.locationLatitude || 0.0,
+          },
+        });
+        return data;
+      });
+
+      console.info(mapped);
+      await this.customerService.createMany(mapped);
+      console.info('customer data seeded successfully');
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
   }
 }
