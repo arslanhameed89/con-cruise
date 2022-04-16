5a727aeb7490823f74417aec82050ab97f81b123
arsal <arsal.hameed@gmail.com>
2022-04-17 00:01:51 +0400

    add drivers listing command

---
+++ src/v1/driver/commands/driver.list.command.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Command } from 'nestjs-command';
+import { DriverService } from '../services/driver.service';
+
+@Injectable()
+export class DriverListCommand {
+  constructor(private readonly driverService: DriverService) {}
+  @Command({
+    command: 'list:cruiser',
+    describe: 'list all existing drivers',
+  })
+  async create() {
+    try {
+      console.info('customer data listed process started');
+      const data = await this.driverService.findAll();
+      console.info(data);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
