95011752ef8477a642fd3e92e51c02f2b9d0c3be
arsal <arsal.hameed@gmail.com>
2022-04-16 22:40:40 +0400

    seeder command added

--- src/v1/customer/customer.module.ts
+++ src/v1/customer/customer.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { CoreModule } from '../../core/core.module';
 import { ProvidersModule } from '../../providers/providers.module';
 import { CustomerController } from './customer.controller';
 import { CustomerProviders } from './providers/customer.providers';
 import { CustomerService } from './services/customer.service';
 import { CustomerRepository } from './repository/customer.repository';
+import { CustomerSeedCommand } from './seed/customer.seed.command';
 
 @Module({
-  imports: [ProvidersModule, CoreModule, ConfigModule, ConfigService],
+  imports: [ProvidersModule, CoreModule, ConfigModule],
   controllers: [CustomerController],
-  providers: [...CustomerProviders, CustomerService, CustomerRepository],
+  providers: [
+    ...CustomerProviders,
+    CustomerService,
+    CustomerRepository,
+    CustomerSeedCommand,
+  ],
   exports: [...CustomerProviders, CustomerService, CustomerRepository],
 })
 export class CustomerModule {}
