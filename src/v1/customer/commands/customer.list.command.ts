633b3643c9b6414dff31cadd55b1c22a5896c464
arsal <arsal.hameed@gmail.com>
2022-04-16 23:50:05 +0400

    command developed for listing existing customer

---
+++ src/v1/customer/commands/customer.list.command.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CustomerService } from '../services/customer.service';
+import { Command } from 'nestjs-command';
+
+@Injectable()
+export class CustomerListCommand {
+  constructor(private readonly customerService: CustomerService) {}
+  @Command({
+    command: 'list:customer',
+    describe: 'list all existing Customers',
+  })
+  async create() {
+    try {
+      console.info('customer data listed process started');
+      const data = await this.customerService.findAll();
+      console.info(data);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
