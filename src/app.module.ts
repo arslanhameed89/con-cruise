020cc6b291241d818469024c6b97b1a5c6ed9e74
arsal <arsal.hameed@gmail.com>
2022-04-16 15:18:18 +0400

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ProvidersModule } from './providers/providers.module';
+import { CoreModule } from './core/core.module';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      envFilePath: ['.env'],
+    }),
+    ProvidersModule,
+    CoreModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
