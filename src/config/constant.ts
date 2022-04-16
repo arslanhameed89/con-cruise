9bd6bda08ce57d9433b87e53e65f373c48800f55
arsal <arsal.hameed@gmail.com>
2022-04-16 13:22:00 +0400

    add base configurations and constants

---
+++ src/config/constant.ts
@@ -0,0 +1,16 @@
+export const constant = {
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+};
+
+export const userRoles = {
+  CUSTOMER: 1,
+  DRIVER: 2,
+  GUEST: 3,
+};
+
+export const userStatus = {
+  ACTIVE: 0,
+  BANNED: 1,
+  UNVERIFIED: 2,
+};
