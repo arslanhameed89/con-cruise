adace8bd7bb9d0d674ef37adea768a4c3e1fd333
arsal <arsal.hameed@gmail.com>
2022-04-17 23:12:22 +0400

    to get matches driver with weightage command created

---
+++ src/v1/driver/commands/driver.match.command.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { Command } from 'nestjs-command';
+import { DriverService } from '../services/driver.service';
+import { CustomerService } from '../../customer/services/customer.service';
+import { isEmpty } from 'lodash';
+
+@Injectable()
+export class DriverMatchCommand {
+  private readonly matchedResult: any[];
+  private readonly matchedDrivers: any[];
+  constructor(
+    private readonly driverService: DriverService,
+    private readonly customerService: CustomerService,
+  ) {
+    this.matchedResult = [];
+    this.matchedDrivers = [];
+  }
+  @Command({
+    command: 'list:match',
+    describe: 'match drivers by algo',
+  })
+  async process() {
+    try {
+      console.info('customer data matching process started');
+      //let count = 0;
+      const data = await this.customerService.findAll();
+
+      for await (const item of data) {
+        const response = await this.driverService.findDriverByDistance(item);
+        if (
+          !isEmpty(response.matchedRides) &&
+          response.matchedRides.length > 0
+        ) {
+          this.matchedResult.push(...response.matchedRides);
+          this.matchedDrivers.push(...response.matchedDrivers);
+        }
+        /*console.info(
+          `===========================${ item.name } :: ${count++}============================================`,
+        );*/
+      }
+      console.info(this.matchedResult, this.matchedDrivers);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+}
