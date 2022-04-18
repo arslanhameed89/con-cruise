654630562ff4fec3a31460cad07c8c368b33d7de
arsal <arsal.hameed@gmail.com>
2022-04-19 01:02:12 +0400

    take port, host from configuration file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const config = app.get(ConfigService);
+  const documentConfig = new DocumentBuilder()
     .setTitle('Con Cruise')
     .setDescription('The con cruise API description')
     .setVersion('1.0')
+    .addServer(config.get('SERVER'))
     .addTag('conCruise')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(config.get('app.PORT'), config.get('app.HOST'));
 }
 bootstrap();
