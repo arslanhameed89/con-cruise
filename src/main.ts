12004e2a56f1e651462956c94bb79479ba8438d3
arsal <arsal.hameed@gmail.com>
2022-04-19 22:49:43 +0400

    fix to server of swagger and validation pipeline

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,11 +12,13 @@ async function bootstrap() {
     .setTitle('Con Cruise')
     .setDescription('The con cruise API description')
     .setVersion('1.0')
-    .addServer(config.get('SERVER'))
+    .addServer(config.get('app.SERVER'))
     .addTag('conCruise')
     .build();
   const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(config.get('app.PORT'), config.get('app.HOST'));
 }
